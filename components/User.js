26fb92c82898ebb08a4a0f160ae2c6ec07e43fcc
Umarjon <urr777777@gmail.com>
2024-07-13 13:26:11 +0500

    fixe

--- components/User.js
+++ components/User.js
@@ -1,7 +1,7 @@
 export function addUser(item) {
     const user = document.createElement('div');
     const span = document.createElement('span')
-    user.classList.add('person-card');
+    user.classList.add('box');
     user.innerHTML = item.name;
     span.innerHTML= 'age: ' + item.age;
     user.append(span)
