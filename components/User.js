d8f524df0e3d35b080c7d054d7457f4d7fb13938
Umarjon <urr777777@gmail.com>
2024-07-13 15:29:01 +0500

    fixed and done

--- components/User.js
+++ components/User.js
@@ -1,12 +1,18 @@
 export function addUser(item) {
-    const user = document.createElement('div');
-    const span = document.createElement('span')
-    user.classList.add('box');
-    user.innerHTML = item.name;
-    span.innerHTML= 'age: ' + item.age;
-    user.append(span)
-    return user;
+    const box = document.createElement('div');
+    const name = document.createElement('span')
+    const age = document.createElement('span')
+    const age_word = document.createElement('span')
+    const cfourcfour = document.createElement('div')
+
+    cfourcfour.classList.add('line')
+    box.classList.add('box');
+    name.innerHTML = item.name;
+    age.innerHTML = item.age;
+    age_word.innerHTML="age"
+
+    box.append(name,cfourcfour)
+    cfourcfour.append(age_word,age)
+    return box;
 }
-
-
 
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { users } from "./lib/db.js";
 import { addUser } from "./components/User.js";
 import { reload } from "./lib/utils.js";
 
-const under_twfi = document.querySelector('#under25');
-const under_fif = document.querySelector('#under50');
-const others = document.querySelector('#over50');
+const under_twfi = document.querySelector('#under_twf');
+const under_fif = document.querySelector('#under_fiv');
+const others = document.querySelector('#over_fiv');
 const userForm = document.forms.namedItem('users_form');
 
 userForm.onsubmit = (e) => {
@@ -16,3 +16,5 @@ userForm.onsubmit = (e) => {
     users.push(user);
     reload(users, addUser, under_twfi, under_fif, others);
 };
+
+
